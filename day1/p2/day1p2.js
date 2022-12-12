8938bc37e9667cc2dafe731ba40dd49c65c206d2
DavidTrindade <dave.ferrari579@gmail.com>
2022-12-12 22:11:59 +0000

    add day 1 part 2

---
+++ day1/p2/day1p2.js
@@ -0,0 +1,19 @@
+const fs = require('fs')
+
+let maxes = Array(3).fill(0);
+let sum = 0;
+
+for (cal of fs.readFileSync('../input.txt', 'utf8').split(/\n/)) {
+    if (cal) {
+        sum += Number(cal)
+    } else {
+        const minIndex = maxes.indexOf(Math.min(...maxes));
+        maxes[minIndex] = Math.max(maxes[minIndex], sum)
+        sum = 0;
+    }
+}
+
+// Sums max array
+const result = maxes.reduce((p, c) => p + c);
+console.log(`Most calories being carried by the top 3 (total): ${result}`);
+console.log(`Individual: ${maxes.join(', ')}`);
